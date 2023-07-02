84e72504f83c60d33c8b2dc736913282a41d54fe
Joel <jrosco@users.noreply.github.com>
2023-07-02 12:48:40 +1000

    Updated sdl_events.py script / example
    show how connection and
    disconnections work with
    `instance_id` and `device_id`
    
    https://metacpan.org/pod/SDL2::joystick#SDL_JoystickGetDeviceInstanceID(-...-)

--- scripts/sdl_events.py
+++ scripts/sdl_events.py
@@ -1,74 +1,76 @@
-import ctypes
 import sdl2
 import sdl2.ext
 
-# Define a ctypes structure for userdata
-class UserData(ctypes.Structure):
-    _fields_ = [("controller", ctypes.c_void_p),
-                ("controller_name", ctypes.c_char_p)]
+# This hint allows joystick events to be processed even when the window is not in focus or minimized.
+# sdl2.ext.HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS = "1"
 
-# Initialize SDL
+# initialize SDL
 sdl2.ext.init()
 
-# Create a list to hold the controller objects
-controllers = []
+# initialize the SDL2 subsystems
+sdl2.SDL_Init(sdl2.SDL_INIT_GAMECONTROLLER | sdl2.SDL_INIT_JOYSTICK)
 
-# Initialize the controller subsystem
-sdl2.SDL_Init(sdl2.SDL_INIT_GAMECONTROLLER)
+# create a instance_id and device_index mapping
+# format: {instance_id: device_index}
+instance_device_index_map = {}
 
-# Get the number of available controllers
-num_controllers = sdl2.SDL_NumJoysticks()
-
-# Open each controller and add it to the list
-for i in range(num_controllers):
-    controller = sdl2.gamecontroller.SDL_GameControllerOpen(i)
-    controllers.append(controller)
-
-# Custom event filter function
-def event_filter(event, userdata):
-    # Handle controller connection event
-    if event.type == sdl2.SDL_CONTROLLERDEVICEADDED:
-        # Get the controller index from the event
-        controller_index = event.cdevice.which
-        
-        # Open the newly connected controller
-        controller = sdl2.gamecontroller.SDL_GameControllerOpen(controller_index)
-
-        # Get the name of the connected controller
-        controller_name = sdl2.gamecontroller.SDL_GameControllerName(controller)
-
-        # Add the controller to the list
-        controllers.append(controller)
-        
-        # Update the variable with controller info
-        userdata.controller = controller
-        userdata.controller_name = controller_name
-
-        # Print the controller info
-        print("Controller connected:", controller_name)
-
-    # Value will be ignored
-    return 0
-
-# Create an instance of UserData
-userdata = UserData()
-
-# Set the event filter with userdata
-# sdl2.SDL_SetEventFilter(sdl2.SDL_EventFilter(event_filter), ctypes.byref(userdata))
-
-# Main loop
+# main loop
 running = True
 while running:
     events = sdl2.ext.get_events()
-        
+
+    # loop the events
     for event in events:
+
+        # get devices current events instance id (cdevice is now a controllers device event)
+        # unlike the controller added event, the controller removed event does not have a device_index
+        # see https://metacpan.org/pod/SDL2::joystick#DESCRIPTION
+        cdevice = event.cdevice.which
+
+        # sdl2 quit event
         if event.type == sdl2.SDL_QUIT:
             running = False
             break
 
-# Close the controllers
-for controller in controllers:
-    sdl2.gamecontroller.SDL_GameControllerClose(controller)
+        # buttons pressed and release event
+        elif event.type == sdl2.SDL_JOYBUTTONDOWN or event.type == sdl2.SDL_JOYBUTTONUP:
 
-# Quit SDL
+            # read button values
+            print(f'button type {event.type} event:{event.cbutton.button} - instance_id:{cdevice}')
+
+            if event.type == sdl2.SDL_JOYBUTTONDOWN and event.cbutton.button == sdl2.SDL_CONTROLLER_BUTTON_A:
+
+                # print the instance_id and device_index mapping when button A is pressed
+                print(f'instance_device_index_map: {instance_device_index_map}')
+
+        # controller added to system
+        elif event.type == sdl2.SDL_CONTROLLERDEVICEADDED:
+
+            # get devices index when controller is first connected
+            device_index = event.cdevice.which
+
+            # set the devices device_index instance
+            # see https://metacpan.org/pod/SDL2::joystick#DESCRIPTION
+            # https://metacpan.org/pod/SDL2::joystick#SDL_JoystickGetDeviceInstanceID(-...-)
+            instance_id = sdl2.SDL_JoystickGetDeviceInstanceID(device_index)
+
+            sdl2.SDL_GameControllerOpen(device_index)
+            print(f'controller added - instance_id:{instance_id}')
+
+            # map instance_id to device_index
+            instance_device_index_map[instance_id] = device_index
+
+        # controller removed from system
+        elif event.type == sdl2.SDL_CONTROLLERDEVICEREMOVED:
+
+            # https://metacpan.org/pod/SDL2::gamecontroller#SDL_GameControllerFromInstanceID(-...-)
+            controller = sdl2.SDL_GameControllerFromInstanceID(cdevice)
+
+            sdl2.SDL_GameControllerClose(controller)
+            print(f'controller removed - instance_id:{cdevice}')
+
+            # remove instance_id from map
+            del instance_device_index_map[cdevice]
+
+# quit SDL
 sdl2.SDL_Quit()
